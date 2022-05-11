34f83313ff2c89d8233767540a0f1643e785603b
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-05-11 19:43:02 +0300

    W3

--- JS/Notebook/W3/Assignment/main.js
+++ JS/Notebook/W3/Assignment/main.js
@@ -1,35 +1,40 @@
+//1
+console.log(10 * 20 - 15 % 3 + 190 + 10 - 400); // 0
 
-// Assignment 1
-// window.alert("Hello");
-// document.createElement("h2").innerHTML="Elzero";
-document.write("<h1>Elzero</h1>");
-document.querySelector("h1").style = "color:blue; font-size: 80px; font-weight: bold; text-align: center; font-family: Arial";
-// document.querySelector("h1").style.fontSize = '80px';
-// document.querySelector("h1").style.fontWeight = 'bold';
-// document.querySelector("h1").style.textAlign = 'center';
-// document.querySelector("h1").style.fontFamily = 'Arial';
-console.log("%cElzero %cWeb %cSchool", "color: red; font-size: 40px", "color: green; font-size: 40px; font-weight: bold", "background-color: blue; font-size: 40px");
-// ------------------------------------------------------
-console.group("Group 1");
-console.log("Level 2");
-console.log("Level 2");
-console.group("Child Group");
-console.log("Level 3");
-console.log("Level 3");
-console.group("Grand Child Group");
-console.log("Level 4");
-console.log("Level 4");
-console.groupEnd();
-console.groupEnd();
-console.groupEnd();
-console.group("Group 2");
-console.log("Level 2");
-console.log("Level 2");
-// ------------------------------------------------------
-console.table(["Elzero", "Ahmed", "Sameh", "Gamal", "Aya"]);
-// ------------------------------------------------------
-// console.log("Iam In Console");
-// document.write("Iam In Page");
-/* console.log("Iam In Console");
-document.write("Iam In Page"); */
-// ------------------------------------------------------
+//2
+
+let num = 3;
+
+// Solution One
+console.log(num * (+true + +true)); // 6
+
+// Solution Two
+console.log(num + num); // 6
+
+// Soultion Three
+console.log(-(-num - num)); // 6
+
+// Soultion Four
+console.log((num % (num + +true)) * num - num); // 6
+
+// Solution Five
+console.log(num * num - num); // 6
+
+// Solution Six
+console.log("Write Your Code Here"); // 6
+
+//3
+
+let num3 = "10";
+
+// Solution One
+console.log(+num3 + +num3); // 20
+
+// Solution Two
+console.log(+num3 * (+true + +true)); // 20
+
+// Solution Three
+console.log((+num3 / (+true + +true)) + (+num3 / (+true + +true)) +(+num3 / (+true + +true)) + (+num3 / (+true + +true))); // 20
+
+// Solution Four
+console.log(++num3 + --num3 - +true); // 20
