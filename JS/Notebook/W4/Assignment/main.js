bb7bc7d96189507a595614e9f5245e654809b250
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-05-12 12:29:43 +0300

    3d commit

--- JS/Notebook/W4/Assignment/main.js
+++ JS/Notebook/W4/Assignment/main.js
@@ -1,35 +1,24 @@
 
 // Assignment 1
-// window.alert("Hello");
-// document.createElement("h2").innerHTML="Elzero";
-document.write("<h1>Elzero</h1>");
-document.querySelector("h1").style = "color:blue; font-size: 80px; font-weight: bold; text-align: center; font-family: Arial";
-// document.querySelector("h1").style.fontSize = '80px';
-// document.querySelector("h1").style.fontWeight = 'bold';
-// document.querySelector("h1").style.textAlign = 'center';
-// document.querySelector("h1").style.fontFamily = 'Arial';
-console.log("%cElzero %cWeb %cSchool", "color: red; font-size: 40px", "color: green; font-size: 40px; font-weight: bold", "background-color: blue; font-size: 40px");
-// ------------------------------------------------------
-console.group("Group 1");
-console.log("Level 2");
-console.log("Level 2");
-console.group("Child Group");
-console.log("Level 3");
-console.log("Level 3");
-console.group("Grand Child Group");
-console.log("Level 4");
-console.log("Level 4");
-console.groupEnd();
-console.groupEnd();
-console.groupEnd();
-console.group("Group 2");
-console.log("Level 2");
-console.log("Level 2");
-// ------------------------------------------------------
-console.table(["Elzero", "Ahmed", "Sameh", "Gamal", "Aya"]);
-// ------------------------------------------------------
-// console.log("Iam In Console");
-// document.write("Iam In Page");
-/* console.log("Iam In Console");
-document.write("Iam In Page"); */
-// ------------------------------------------------------
+
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log(); // 100000
+console.log(1e5); // 100000
+console.log(10 ** 5); // 100000
+console.log(100000.0); // 100000
+console.log(Math.round(100000.2)); // 100000
+console.log(Math.round(99999.6)); // 100000
+console.log(Math.ceil(99999.2)); // 100000
+console.log(Math.floor(100000.9)); // 100000
+console.log(Math.trunc(100000.9)); // 100000
+console.log(Math.pow(10, 5)); // 100000
+console.log(Math.min(100000, 1000000, 10000000)); // 100000
+console.log(Math.max(100000, 1000, 10000)); // 100000
+console.log(parseInt("100000 Osama")); // 100000
+console.log(+"100000"); // 100000
+
