f80f2efc859ecb14051eb06ff71ab8355af651f9
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-05-12 16:36:17 +0300

    Assign 23 2 26

--- JS/Notebook/W4/Assignment/main.js
+++ JS/Notebook/W4/Assignment/main.js
@@ -22,3 +22,33 @@ console.log(Math.max(100000, 1000, 10000)); // 100000
 console.log(parseInt("100000 Osama")); // 100000
 console.log(+"100000"); // 100000
 
+//Q2:
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+//Q3:
+
+console.log(Number.MAX_SAFE_INTEGER.toString().length); //16
+
+//Q4:
+let myVar = "100.56789 Views";
+
+console.log(parseInt(myVar)); // 100
+console.log(+(parseFloat(myVar).toFixed(2))); // 100.57
+
+//Q5
+let num5 = 10;
+
+console.log(+Number.isInteger(num5) + +Number.isInteger(num5)); // 2
+
+//Q6
+let flt = 10.4;
+
+console.log(Math.round(flt)); // 10
+console.log(Math.floor(flt)); // 10
+console.log(Math.trunc(flt)); // 10
+console.log(parseInt(flt)); // 10
+console.log(+(flt.toFixed(0))); // 10
+
+//Q7
+console.log(Math.round(Math.random()*4)); // 0 || 1 || 2 || 3 || 4
