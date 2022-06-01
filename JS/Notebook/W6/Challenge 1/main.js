0b936a44df441141b10990c54d6377c0d96c0ff6
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-06-02 02:39:46 +0300

    week 6 Challenge

--- JS/Notebook/W6/Challenge 1/main.js
+++ JS/Notebook/W6/Challenge 1/main.js
@@ -1,99 +1,24 @@
 /*
-  Challenge 2
+  Challenge 1
 */
 
 /*
-  Switch Challenge
+  Array Challenge
 */
 
-let job = "Manager";
-let salary = 0;
+let zero = 0;
 
-if (job === "Manager") {
-    salary = 8000;
-} else if (job === "IT" || job === "Support") {
-    salary = 6000;
-} else if (job === "Developer" || job === "Designer") {
-    salary = 7000;
-} else {
-    salary = 4000;
-}
+let counter = 3;
 
-/*
-  Answer
-*/
+let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
 
-switch (job) {
-    case "Manager":
-        salary = 8000;
-        break;
-    case "IT" || "Support":
-        salary = 6000;
-        break;
-    case "Developer" || "Designer":
-        salary = 7000;
-        break;
-    default:
-        salary = 4000;
-        break;
-}
+// Write Code Here
+my = my.slice(zero, ++counter).reverse();
+console.log(my); // ["Osama", "Elham", "Mazero", "Ahmed"];
 
-console.log(salary);
+console.log(my.slice(++zero, --counter)); // ["Elham", "Mazero"]
 
-/*
-  If Challenge
-*/
+elzero = my[zero].substring(--zero, --counter) + my[counter].substring(++zero + zero);
+console.log(elzero); // "Elzero"
 
-let holidays = 0;
-let money = 0;
-
-switch (holidays) {
-    case 0:
-        money = 5000;
-        console.log(`My Money is ${money}`);
-        break;
-    case 1:
-    case 2:
-        money = 3000;
-        console.log(`My Money is ${money}`);
-        break;
-    case 3:
-        money = 2000;
-        console.log(`My Money is ${money}`);
-        break;
-    case 4:
-        money = 1000;
-        console.log(`My Money is ${money}`);
-        break;
-    case 5:
-        money = 0;
-        console.log(`My Money is ${money}`);
-        break;
-    default:
-        money = 0;
-        console.log(`My Money is ${money}`);
-}
-
-/*
-  Answer
-*/
-
-if (holidays === 0) {
-    money = 5000;
-    console.log(`My Money is ${money}`);
-} else if (holidays === 1 || holidays === 2) {
-    money = 3000;
-    console.log(`My Money is ${money}`);
-} else if (holidays === 3) {
-    money = 2000;
-    console.log(`My Money is ${money}`);
-} else if (holidays === 4) {
-    money = 1000;
-    console.log(`My Money is ${money}`);
-} else if (holidays === 5) {
-    money = 0;
-    console.log(`My Money is ${money}`);
-} else {
-    money = 0;
-    console.log(`My Money is ${money}`);
-}
+console.log(elzero[counter + counter] + elzero.substring(counter + ++counter).toUpperCase()); // "rO"
