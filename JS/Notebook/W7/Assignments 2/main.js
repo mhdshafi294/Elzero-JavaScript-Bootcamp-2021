bcdb749cfa9f5ad283e542b5296165c73452da83
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-06-02 22:48:40 +0300

    week 7 Assingment2

--- JS/Notebook/W7/Assignments 2/main.js
+++ JS/Notebook/W7/Assignments 2/main.js
@@ -1,3 +1,35 @@
 /*
   Assignment 2
 */
+
+let friends = ["Ahmed", "Sayed", "Ali", 1, 2, "Mahmoud", "Amany"];
+let index = 0;
+let counter = 0;
+let letter = "a";
+
+// Output
+// "1 => Sayed"
+// "2 => Mahmoud"
+
+//Answer
+// for (let element of friends) {
+//     if (typeof(element) === "number") {
+//         continue;
+//     }
+//     if (element.toLowerCase().startsWith(letter)) {
+//         continue;
+//     } else {
+//         console.log(`${++counter} => ${element}`)
+
+//     }
+// }
+
+for (let i = index; i < friends.length; i++) {
+    if (typeof(friends[i]) === "number") {
+        continue;
+    } else if (friends[i].toLowerCase().startsWith(letter)) {
+        continue;
+    } else {
+        console.log(`${++counter} => ${friends[i]}`)
+    }
+}
