d8ebbbdc73b113f2c847f3c7836a13fcb7a3c42a
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-06-02 22:37:39 +0300

    week 7 Assingment

--- JS/Notebook/W7/Assignments 1/main.js
+++ JS/Notebook/W7/Assignments 1/main.js
@@ -1,99 +1,161 @@
 /*
-  Challenge 2
+  Assignment 1
 */
 
-/*
-  Switch Challenge
-*/
+//Q1
 
-let job = "Manager";
-let salary = 0;
+/* let start = 10;
+let end = 100;
+let exclude = 40;
 
-if (job === "Manager") {
-    salary = 8000;
-} else if (job === "IT" || job === "Support") {
-    salary = 6000;
-} else if (job === "Developer" || job === "Designer") {
-    salary = 7000;
-} else {
-    salary = 4000;
+// Output
+10
+20
+30
+50
+60
+70
+80
+90
+100
+
+// Answer
+
+for (let i = start; i <= end; i += start) {
+    if (i == exclude) {
+        continue;
+    } else {
+        console.log(i);
+    }
+} */
+
+//Q2
+/* let start = 10;
+let end = 0;
+let stop = 3;
+
+// Output
+10
+09
+08
+07
+06
+05
+04
+03
+// Answer
+for (let i = start; i >= stop; i--) {
+    if (i < start) {
+        console.log(end, i);
+    } else {
+        console.log(i);
+    }
+} */
+
+//Q3
+/* let start = 1;
+let end = 6;
+let breaker = 2;
+
+// Output
+// 1
+// --2
+// --4
+// 2
+// --2
+// --4
+// 3
+// --2
+// --4
+// 4
+// --2
+// --4
+// 5
+// --2
+// --4
+// 6
+// --2
+// --4
+
+// Answer
+for (let i = start; i <= end; i++) {
+    console.log(`${i}`);
+    console.log(`--${breaker}`);
+    console.log(`--${end - breaker}`);
+} */
+
+//Q4
+/* let index = 10;
+let jump = 2;
+let end = 0;
+
+for (;;) {
+    // Write Your Code Here
+    console.log(index);
+    index = index - jump;
+    if (index === jump) {
+        break;
+    }
 }
 
-/*
-  Answer
-*/
+// Output
+// 10
+// 8
+// 6
+// 4 */
 
-switch (job) {
-    case "Manager":
-        salary = 8000;
-        break;
-    case "IT" || "Support":
-        salary = 6000;
-        break;
-    case "Developer" || "Designer":
-        salary = 7000;
-        break;
-    default:
-        salary = 4000;
-        break;
+
+//Q5
+/* let friends = ["Ahmed", "Sayed", "Eman", "Mahmoud", "Ameer", "Osama", "Sameh"];
+let letter = "a";
+
+// Output
+// "1 => Sayed"
+// "2 => Eman"
+// "3 => Mahmoud"
+// "4 => Osama"
+// "5 => Sameh"
+
+//Answer
+counter = letter.length;
+for (let element of friends) {
+    if (element.toLocaleLowerCase().startsWith(letter)) {
+        continue;
+    } else {
+        console.log(`${counter} => ${element}`)
+        counter++
+    }
+
+} */
+
+//Q6
+/* let start = 0;
+let swappedName = "elZerO";
+let newName = "";
+
+for (let i = start; i < swappedName.length; i++) {
+    if (swappedName[i] === swappedName[i].toUpperCase()) {
+        newName += swappedName[i].toLowerCase();
+    } else {
+        newName += swappedName[i].toUpperCase();
+        // console.log(swappedName[i].toUpperCase());
+    }
 }
+console.log(newName); */
 
-console.log(salary);
+//Q7
+let start = 0;
+let mix = [1, 2, 3, "A", "B", "C", 4];
 
-/*
-  If Challenge
-*/
+// Output
+// 2
+// 3
+// 4
 
-let holidays = 0;
-let money = 0;
+//Answer
 
-switch (holidays) {
-    case 0:
-        money = 5000;
-        console.log(`My Money is ${money}`);
-        break;
-    case 1:
-    case 2:
-        money = 3000;
-        console.log(`My Money is ${money}`);
-        break;
-    case 3:
-        money = 2000;
-        console.log(`My Money is ${money}`);
-        break;
-    case 4:
-        money = 1000;
-        console.log(`My Money is ${money}`);
-        break;
-    case 5:
-        money = 0;
-        console.log(`My Money is ${money}`);
-        break;
-    default:
-        money = 0;
-        console.log(`My Money is ${money}`);
-}
-
-/*
-  Answer
-*/
-
-if (holidays === 0) {
-    money = 5000;
-    console.log(`My Money is ${money}`);
-} else if (holidays === 1 || holidays === 2) {
-    money = 3000;
-    console.log(`My Money is ${money}`);
-} else if (holidays === 3) {
-    money = 2000;
-    console.log(`My Money is ${money}`);
-} else if (holidays === 4) {
-    money = 1000;
-    console.log(`My Money is ${money}`);
-} else if (holidays === 5) {
-    money = 0;
-    console.log(`My Money is ${money}`);
-} else {
-    money = 0;
-    console.log(`My Money is ${money}`);
+for (let i = ++start; i < mix.length; i++) {
+    if (typeof mix[i] === "number") {
+        console.log(mix[i]);
+    }
 }
