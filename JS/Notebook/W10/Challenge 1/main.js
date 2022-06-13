f8d150b99cdbb8b3dc2f297390ebad0f6f086d68
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-06-13 23:46:36 +0300

    week 10 Challange

--- JS/Notebook/W10/Challenge 1/main.js
+++ JS/Notebook/W10/Challenge 1/main.js
@@ -1,3 +1,38 @@
 /*
   Challenge 1
 */
+
+/*
+  Higher Order Functions Challenges
+
+  You Can Use
+  - ,
+  - _
+  - Space
+  - True => 1 => One Time Only In The Code
+
+  You Cannot Use
+  - Numbers
+  - Letters
+
+  - You Must Use [Filter + Map + Reduce + Your Knowledge]
+  - Order Is Not Important
+  - All In One Chain
+
+*/
+
+let myString = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+let solution = myString
+    .split(",")
+    .filter(function(el) {
+        return isNaN(parseInt(el)) && el !== "Z";
+    })
+    .map(function(el) {
+        return el === "_" ? " " : el[0];
+    })
+    .reduce(function(acc, current) {
+        return `${acc}${current}`;
+    });
+
+console.log(solution); // Elzero Web School
