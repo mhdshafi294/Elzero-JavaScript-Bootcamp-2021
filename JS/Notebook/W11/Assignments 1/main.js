33ac5b0c908a9a926c24424bd2ab37f144cc3ddd
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-06-15 15:19:33 +0300

    week 11 Assignment Q3

--- JS/Notebook/W11/Assignments 1/main.js
+++ JS/Notebook/W11/Assignments 1/main.js
@@ -55,31 +55,32 @@
 
 // console.log(objMethodFour.property); // "Method Four"
 
-//Q3
-let a = 1;
+// //Q3
+// let a = 1;
 
-let threeNums = {
-  b: 2,
-  c: 3,
-  d: 4,
-};
+// let threeNums = {
+//     b: 2,
+//     c: 3,
+//     d: 4,
+// };
 
-let twoNums = {
-  e: 5,
-  f: 6,
-};
+// let twoNums = {
+//     e: 5,
+//     f: 6,
+// };
 
-// Create Your Object Here in One Line
+// // Create Your Object Here in One Line
+// let finalObject = Object.assign({ a }, threeNums, twoNums);
 
-console.log(finalObject);
+// console.log(finalObject);
 
-/*
-  a: 1
-  b: 2
-  c: 3
-  d: 4
-  e: 5
-  f: 6
-*/
+// /*
+//   a: 1
+//   b: 2
+//   c: 3
+//   d: 4
+//   e: 5
+//   f: 6
+// */
 
 //Q4
