b22a92139ac53571189325d686ae7de037b9f6aa
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2022-06-15 15:14:01 +0300

    week 11 Assignment firstHalf

--- JS/Notebook/W11/Assignments 1/main.js
+++ JS/Notebook/W11/Assignments 1/main.js
@@ -1,3 +1,85 @@
 /*
   Assignment 1
 */
+
+
+//Q1
+// Create Your Object Here
+// let member = {
+//     name: "Elzero",
+//     age: 38,
+//     country: "Egypt",
+//     fullDetails: function() {
+//         console.log(`My Name Is ${this.name}, My Age Is ${this.age}, I Live in ${this.country}`);
+//     },
+// };
+
+
+// console.log(member.name); // Elzero
+// console.log(member.age); // 38
+// console.log(member.country); // Egypt
+// console.log(member.fullDetails());
+// My Name Is Elzero, My Age Is 38, I Live in Egypt
+
+//Q2
+// Method One
+// Create Your Object Here
+// let objMethodOne = {
+//     property: "Method One",
+// };
+
+// console.log(objMethodOne.property); // "Method One"
+
+// // Method Two
+// // Create Your Object Here
+// let objMethodTwo = new Object({
+//     property: "Method Two",
+// });
+
+// console.log(objMethodTwo.property); // "Method Two"
+
+
+// // Method Three
+// // Create Your Object Here
+// let objMethodThree = Object.create({
+//     property: "Method Three",
+// });
+
+// console.log(objMethodThree.property); // "Method Three"
+
+
+// // Method Four
+// // Create Your Object Here
+// let objMethodFour = Object.assign({}, { property: "Method Four" });
+
+
+// console.log(objMethodFour.property); // "Method Four"
+
+//Q3
+let a = 1;
+
+let threeNums = {
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+let twoNums = {
+  e: 5,
+  f: 6,
+};
+
+// Create Your Object Here in One Line
+
+console.log(finalObject);
+
+/*
+  a: 1
+  b: 2
+  c: 3
+  d: 4
+  e: 5
+  f: 6
+*/
+
+//Q4
